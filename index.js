e4210c2db0942c5e4c2980762a6208f6e982248d
fullstackgeeks <calebsotani@gmail.com>
2024-02-20 22:08:20 +0100

    adding dist directory 2

--- index.js
+++ index.js
@@ -391,7 +391,7 @@ app.use(unknownEndpoint)
 
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, ()=> {console.log(`Server running on port', ${RT}`)})
+app.listen(PORT, ()=> {console.log(`Server running on port', ${PORT}`)})
 
 
 
